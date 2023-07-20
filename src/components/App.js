e78b134e7843e19438fd5c56a8f5fa787146973b
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-20 07:05:09 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,49 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [listItems, setListItem] = useState([]);
 
-  const[inputText, setInputText] = useState("")
-  const[listItems, setListItem] = useState([])
-
-  function addListItem(){
-    setListItem(prevItem => {
-      return [...prevItem, inputText]
-    })
-    setInputText("")
+  function addListItem() {
+    setListItem((prevItem) => {
+      return [...prevItem, inputText];
+    });
+    setInputText("");
   }
 
-  function hndleDeleteBtn(index){
-    const newListItem = listItems.splice(index,1)
-    setListItem(newListItem)
+  function hndleDeleteBtn(index) {
+    console.log(index);
+    let newlistItem = [...listItems];
+    newlistItem.splice(index, 1);
+    // const newListItem = listItems.splice(index, 1);
+    // console.log(newListItem)
+    setListItem(newlistItem);
   }
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        
-        <h3>To-Do List</h3>
-        <div className="inputBtnDiv">
-        <input type="text" onChange={(e)=>setInputText(e.target.value)}  value={inputText}/>
-        <button onClick={addListItem}>Add</button>
-        </div>
-        <ul>
-        {listItems.map((item,index) => (
-          <li key={index}>{item}
-          <button onClick={() => hndleDeleteBtn(index)}>Delete</button>
-          </li>
-          
-          )) }
-        
-        </ul>
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <h3>To-Do List</h3>
+      <div className="inputBtnDiv">
+        <input
+          type="text"
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+        />
+        <button onClick={addListItem}>Add</button>
+      </div>
+      <ul>
+        {listItems.map((item, index) => (
+          <li key={index}>
+            {item + "-" + index}
+            <button onClick={() => hndleDeleteBtn(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
